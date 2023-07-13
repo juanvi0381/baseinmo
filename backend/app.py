4708d02c589446c6c23a47f8cca11c710901e613
Vicente Suarez <123718440+juanvi0381@users.noreply.github.com>
2023-07-13 17:04:58 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,15 @@
+from flask import Flask ,jsonify ,request
+# del modulo flask importar la clase Flask y los métodos jsonify,request
+from flask_cors import CORS       # del modulo flask_cors importar CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+app=Flask(__name__)  # crear el objeto app de la clase Flask
+CORS(app) #modulo cors es para que me permita acceder desde el frontend al backend
 
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://Juanvi:Inmobase@Juanvi.mysql.pythonanywhere-services.com/Juanvi$default'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #none
+
+db= SQLAlchemy(app)   #crea el objeto db de la clase SQLAlquemy
+ma=Marshmallow(app)   #crea el objeto ma de de la clase Marshmallow
+
+from controladores import *
